2d30197ef2f4cae62950f6d22c7b81b047a6b4d9
Semen Gricenko <semengricenko@gmail.com>
2020-07-16 16:35:48 +0500

    Now log file size is 128 MB

--- src/LogConstants.h
+++ src/LogConstants.h
@@ -2,5 +2,5 @@
 
 #include <string>
 
-inline constexpr uint32_t logFileSize = 256 * 1024 * 1024;	//256 MB
+inline constexpr uint32_t logFileSize = 128 * 1024 * 1024;	//128 MB
 inline constexpr std::string_view parentDirectory = "logs";
