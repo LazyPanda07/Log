d022e28865ae93c71acca003c05135060c7a79f1
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-02-29 16:01:15 +0500

    Fix

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,3 +1,6 @@
+#include <filesystem>
+#include <iostream>
+
 #include "gtest/gtest.h"
 
 #include "Log.h"
@@ -5,6 +8,9 @@
 TEST(Log, Logging)
 {
 	Log::init();
+
+    std::cout << Log::getCurrentLogFilePath() << std::endl;
+    std::cout << std::filesystem::exists(Log::getCurrentLogFilePath()) << std::endl;
 
     Log::info("Information message on line {}", __LINE__);
 
@@ -20,22 +26,22 @@ TEST(Log, Logging)
     std::getline(in, tem);
 
     ASSERT_TRUE(tem.find("INFO") != std::string::npos);
-    ASSERT_TRUE(tem.find("Information message on line 9") != std::string::npos);
+    ASSERT_TRUE(tem.find("Information message on line 15") != std::string::npos);
 
     std::getline(in, tem);
 
     ASSERT_TRUE(tem.find("WARNING") != std::string::npos);
-    ASSERT_TRUE(tem.find("Warning message on line 11") != std::string::npos);
+    ASSERT_TRUE(tem.find("Warning message on line 17") != std::string::npos);
 
     std::getline(in, tem);
 
     ASSERT_TRUE(tem.find("ERROR") != std::string::npos);
-    ASSERT_TRUE(tem.find("Error message on line 13") != std::string::npos);
+    ASSERT_TRUE(tem.find("Error message on line 19") != std::string::npos);
 
     std::getline(in, tem);
 
     ASSERT_TRUE(tem.find("FATAL_ERROR") != std::string::npos);
-    ASSERT_TRUE(tem.find("Fatal error message on line 15") != std::string::npos);
+    ASSERT_TRUE(tem.find("Fatal error message on line 21") != std::string::npos);
 }
 
 int main(int argc, char** argv)
